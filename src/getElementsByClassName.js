c2e7c0f2bb7d8f97268e292b621038d79c74182d
Seva Mouler <vpmouler@gmail.com>
2017-08-08 21:59:17 -0700

    worked on chome dev tools on a custom simple HTML file I made from sublime. Got a clear understanding of the actual structure of DOM when there are a lot of nodes, and logging .childNodes, [i], .nodeValue, .classList, .contains. While cross referencing MDN and implementing on sublime. Will see if I can use an alternative method to .classList, maybe sometihng with className

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var documentBody = document.body;
+  var arrayOfElements = [];
+  var recursion = function(node) {
+  	if ( node.classList && node.classList.contains(className) ) {
+  		arrayOfElements.push(node);
+  	}
+  	node.childNodes.forEach(function(ele) {
+  		recursion(ele);
+  	});
+  }
+  recursion(documentBody);
+  return arrayOfElements;
 };
+
+// var array = ['seva','bert',1];
+// var object = {name: 'seva', age: 23};
+
+// var elements = document.getElementsByClassName('seva')
